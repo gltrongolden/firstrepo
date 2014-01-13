268d345c50079b38c15ea8330dcbf7214068f674
ma <ma@fosslabs.ru>
2014-01-13 14:15:46 +0400

    initial

---
+++ Test.java
@@ -0,0 +1,12 @@
+//ahahah
+//no seriously
+public class Test {
+	public static void main (String [] args) {
+		int k = Integer.parseInt(args[0]);
+		int s = 1;
+		for (int i = 1; i<=k; i++) {
+			s = s * i;
+		}
+		System.out.println(s);
+	}
+}
